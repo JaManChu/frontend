6293deb1dfe561d071b032184164c5f4fc89cb6c
sol2588 <dsk2588@gmail.com>
2024-09-29 01:21:05 +0900

    Design: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+
 import Onboard from './views/onboard.tsx';
 import Login from './views/login.tsx';
 import Signup from './views/signup.tsx';
 import Mypage from './views/mypage.tsx';
 import GuestHome from './views/guestHome.tsx';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header.tsx';
 
 function App() {
     return (
@@ -13,8 +15,11 @@ function App() {
                 <Route path="/" element={<Onboard />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/guesthome" element={<GuestHome />} />
+                {/* Header 추가해야하는 컴포넌트는 아래에 작성 */}
+                <Route element={<Header />}>
+                    <Route path="/mypage" element={<Mypage />} />
+                    <Route path="/guesthome" element={<GuestHome />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FiLogOut, FiUser } from 'react-icons/fi';
+
+const HeaderSection = styled.section`
+    height: 10vh;
+    padding: 16px;
+    font-size: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const Logo = styled.img`
+    flex-grow: 2;
+`;
+const MenuList = styled.ul`
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    justify-content: flex-end;
+    display: flex;
+    flex-grow: 1;
+`;
+const MenuItem = styled.li<{ active: boolean }>`
+    margin: 10px;
+    cursor: pointer;
+    &:hover {
+        border-bottom: 3px solid #efb63f;
+    }
+    border-bottom: ${(props) => (props.active ? '3px solid #efb63f' : 'none')};
+`;
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    margin: 10px;
+    color: inherit;
+`;
+const UserAction = styled.div`
+    flex-grow: 1;
+    cursor: pointer;
+`;
+
+export default function Header() {
+    const [isActive, setIsActive] = useState<string>('Home');
+    const menuItems = [
+        { name: 'Home', to: '/' },
+        { name: 'Recommend', to: '/recipes/recommended' },
+        { name: 'Popular', to: '/recipes/popular' },
+        { name: 'Latest', to: '/recipes/latest' },
+    ];
+    const handleClickMenu = (menu: string): void => {
+        setIsActive(menu);
+    };
+
+    return (
+        <>
+            <HeaderSection>
+                <Logo src="" alt="LOGO" />
+                <MenuList>
+                    {menuItems.map((item, idx) => (
+                        <MenuItem key={item.name + idx} active={isActive == item.name} onClick={() => handleClickMenu(item.name)}>
+                            <StyledLink to={`${item.to}`}>{item.name}</StyledLink>
+                        </MenuItem>
+                    ))}
+                </MenuList>
+                <UserAction>
+                    <StyledLink to="/">
+                        <FiLogOut />
+                    </StyledLink>
+                    <StyledLink to="/mypage">
+                        <FiUser />
+                    </StyledLink>
+                </UserAction>
+            </HeaderSection>
+            <Outlet />
+        </>
+    );
+}

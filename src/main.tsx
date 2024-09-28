6deead06c52e276ad1559a8bb3a491a879403072
kkim_se <sehun910124@naver.com>
2024-09-28 20:43:11 +0900

    design/#11/onboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Onboard from "./views/onboard.tsx";
+import Login from "./views/login.tsx";
+import Signup from "./views/signup.tsx";
+import Mypage from "./views/mypage.tsx";
+import GuestHome from "./views/guestHome.tsx";
+import "./index.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/guesthome" element={<GuestHome />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/views/guestHome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuestHome: React.FC = () => {
+  return (
+    <>
+      <div>비회원용 홈페이지입니다.</div>
+    </>
+  );
+};
+
+export default GuestHome;

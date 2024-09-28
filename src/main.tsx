7931d88e34bdf75d7a7c9089647a66b89116ebc9
kkim_se <sehun910124@naver.com>
2024-09-28 23:15:02 +0900

    design/#16/signup

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Onboard from './views/onboard.tsx';
-import Login from './views/login.tsx';
-import Signup from './views/signup.tsx';
-import Mypage from './views/mypage.tsx';
-import GuestHome from './views/guestHome.tsx';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Onboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/guesthome" element={<GuestHome />} />
-            </Routes>
-        </BrowserRouter>
+        <App />
     </StrictMode>,
 );

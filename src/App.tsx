8908863ef1f928ea8a3dd85aa776690f22b73194
sol2588 <dsk2588@gmail.com>
2024-09-29 10:38:41 +0900

    Fix: Body margin 삭제 onBoard 컴포넌트 이미지 파일 영역 초과 Header 컴포넌트 영역수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Onboard from './views/onboard.tsx';
 import Login from './views/login.tsx';
 import Signup from './views/signup.tsx';
 import Mypage from './views/mypage.tsx';
 import GuestHome from './views/guestHome.tsx';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header.tsx';
+import Main from './views/Main/Main.tsx';
 
 function App() {
     return (
@@ -15,10 +14,11 @@ function App() {
                 <Route path="/" element={<Onboard />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                {/* Header 추가해야하는 컴포넌트는 아래에 작성 */}
+                {/* Header 추가해야하는 컴포넌트는 Header 아래에 작성 */}
                 <Route element={<Header />}>
                     <Route path="/mypage" element={<Mypage />} />
                     <Route path="/guesthome" element={<GuestHome />} />
+                    <Route path="/main" element={<Main />} />
                 </Route>
             </Routes>
         </BrowserRouter>

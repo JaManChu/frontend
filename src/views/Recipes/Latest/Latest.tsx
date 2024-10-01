005676f685c1d9fae6a22ee8d411573ec6450ac6
sol2588 <dsk2588@gmail.com>
2024-10-01 18:16:59 +0900

    Chore: fakeData.ts파일 분리

--- src/views/Recipes/Latest/Latest.tsx
+++ src/views/Recipes/Latest/Latest.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import RecipeList from '../Recipe/RecipeList';
 import RecipePageHeader from '../Recipe/RecipePageHeader';
 import axios from 'axios';
+import fakeData from '../../../fakeData/recipeFake.js';
 
 interface RecipeLimitProps {
     limit?: number;
@@ -17,53 +18,6 @@ export default function Latest({ limit, isMain }: RecipeLimitProps) {
             } catch (err) {}
         };
     }, [recipes]);
-    const fakeData = [
-        {
-            title: 'first',
-            id: '1',
-            image: '',
-            time: '60min',
-            level: '상',
-            rate: '4.5',
-            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quasi veritatis voluptatem voluptatibus provident modi rerum temporibus labore mollitia omnis sint, dicta blanditiis culpa, pariatur voluptatum unde? Tempore, incidunt eius!',
-        },
-        {
-            title: 'second',
-            id: '2',
-            image: '',
-            time: '45min',
-            level: '상',
-            rate: '1.5',
-            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quasi veritatis voluptatem voluptatibus provident modi rerum temporibus labore mollitia omnis sint, dicta blanditiis culpa, pariatur voluptatum unde? Tempore, incidunt eius!',
-        },
-        {
-            title: 'third',
-            id: '3',
-            image: '',
-            time: '20min',
-            level: '상',
-            rate: '1.5',
-            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quasi veritatis voluptatem voluptatibus provident modi rerum temporibus labore mollitia omnis sint, dicta blanditiis culpa, pariatur voluptatum unde? Tempore, incidunt eius!',
-        },
-        {
-            title: 'fourth',
-            id: '4',
-            image: '',
-            time: '10min',
-            level: '상',
-            rate: '3.5',
-            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quasi veritatis voluptatem voluptatibus provident modi rerum temporibus labore mollitia omnis sint, dicta blanditiis culpa, pariatur voluptatum unde? Tempore, incidunt eius!',
-        },
-        {
-            title: 'fifth',
-            id: '5',
-            image: '',
-            time: '6min',
-            level: '상',
-            rate: '5.0',
-            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quasi veritatis voluptatem voluptatibus provident modi rerum temporibus labore mollitia omnis sint, dicta blanditiis culpa, pariatur voluptatum unde? Tempore, incidunt eius!',
-        },
-    ];
 
     return (
         <>

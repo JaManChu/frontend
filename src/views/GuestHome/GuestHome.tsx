54054fc65f67f1cafd8f87aec73641b113667199
kkim_se <sehun910124@naver.com>
2024-10-03 23:03:39 +0900

    [refactor] mypage,guesthome

--- src/views/GuestHome/GuestHome.tsx
+++ src/views/GuestHome/GuestHome.tsx
@@ -1,10 +1,9 @@
-import React, { ChangeEvent, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { CiSearch } from 'react-icons/ci';
 import { useNavigate } from 'react-router-dom';
 import MainLogo from '../../assets/img/spoon.jpg';
-import axios from 'axios';
-
+import { useSearchInput } from './hooks/useSearchInput';
 const GuestContainer = styled.section`
     background-color: #f5f4f3;
     height: 100%;
@@ -167,38 +166,7 @@ const VisitedSection = styled.div`
     }
 `;
 const GuestHome: React.FC = () => {
-    const [searched, setSearched] = useState<string>('');
-    const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
-    const [message, setMessage] = useState<string>('');
-
-    const changeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const search = e.target.value;
-        setSelectedIngredients(search.split(' '));
-        setSearched(search);
-    };
-
-    const handleSubmit = async () => {
-        if (searched === '') {
-            alert('검색어를 입력해주세요');
-        } else {
-            alert(selectedIngredients);
-        }
-        try {
-            const response = await axios.get('/recipes/search');
-            // ! 데이터 들어오는 구조 확인 - 데이터 받아오면 setSelectedIngredients 데이터 가공해서 화면 렌더링 처리
-        } catch (err: any) {
-            console.log(err);
-            setMessage(err);
-        }
-    };
-
-    const handleActiveEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            handleSubmit();
-        }
-    };
-
+    const { searched, changeInputHandler, handleActiveEnter, handleSubmit } = useSearchInput();
     //추후 api통신을통해 받은 값으로 대체
     const LatestInfo = {
         img: 'https://thumb.ac-illust.com/73/7387030e5a5600726e5309496353969a_t.jpeg',
---
+++ src/views/GuestHome/hooks/useSearchInput.ts
@@ -0,0 +1,45 @@
+import { useState, ChangeEvent } from 'react';
+import axios from 'axios';
+export const useSearchInput = () => {
+    const [searched, setSearched] = useState<string>('');
+    const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
+    const [message, setMessage] = useState<string>('');
+
+    const changeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const search = e.target.value;
+        setSelectedIngredients(search.split(' '));
+        setSearched(search);
+    };
+
+    const handleSubmit = async () => {
+        if (searched === '') {
+            alert('검색어를 입력해주세요');
+        } else {
+            alert(selectedIngredients);
+        }
+        try {
+            const response = await axios.get('/recipes/search');
+            // ! 데이터 들어오는 구조 확인 - 데이터 받아오면 setSelectedIngredients 데이터 가공해서 화면 렌더링 처리
+        } catch (err: any) {
+            console.log(err);
+            setMessage(err);
+        }
+    };
+
+    const handleActiveEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSubmit();
+        }
+    };
+
+    return {
+        searched,
+        setSearched,
+        message,
+        setMessage,
+        changeInputHandler,
+        handleSubmit,
+        handleActiveEnter,
+    };
+};

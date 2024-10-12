34b9b0f5c9e4da107d840ada4d6c180101eb91e1
kkim_se <sehun910124@naver.com>
2024-10-12 13:51:41 +0900

    [feat] MypageAPI

---
+++ src/views/Mypage/hooks/getUserInfo.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const GetUserInfo = () => {
+    const token = sessionStorage.getItem('token');
+    const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState({ email: '', nickname: '', img: '' });
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/login');
+            return;
+        } else {
+            const fetchUserInfo = async () => {
+                try {
+                    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/users`, {
+                        headers: {
+                            'Access-Token': `Bearer ${token}`,
+                        },
+                    });
+                    const { email, nickname } = response.data.data;
+                    setUserInfo({
+                        email: email,
+                        nickname: nickname,
+                        img: 'https://thumb.ac-illust.com/73/7387030e5a5600726e5309496353969a_t.jpeg',
+                    });
+                } catch (error) {
+                    console.error('유저정보를 불러오는 데 실패했습니다', error);
+                }
+            };
+            fetchUserInfo();
+        }
+    }, [token, navigate]);
+
+    return {
+        userInfo,
+        setUserInfo,
+    };
+};

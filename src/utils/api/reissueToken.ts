48cf1a0010f6a5e73fb82c47485f661f66ddb669
sol2588 <dsk2588@gmail.com>
2024-10-17 01:24:58 +0900

    Fix: interceptor - 로그인 테스트

--- src/utils/api/reissueToken.ts
+++ src/utils/api/reissueToken.ts
@@ -1,15 +1,8 @@
 import instance from './instance';
-import { store } from '../../redux/store/store';
 
 const reissueToken = async () => {
     try {
-        const token = store.getState().user.value.token; // 기존 토큰 가져오기
-        const response = await instance.get('/auth/token/refresh', {
-            headers: {
-                'access-token': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
+        const response = await instance.get('/auth/token/refresh');
 
         if (response.data.code == 'OK') {
             const token = response.headers['access-token'];

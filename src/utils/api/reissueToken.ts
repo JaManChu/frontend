2fc1084cf334695db3c82ff9923433a4448c42ec
sol2588 <dsk2588@gmail.com>
2024-10-16 15:50:13 +0900

    Feat: Access Token 갱신 로직 creatComments Api에 도입

---
+++ src/utils/api/reissueToken.ts
@@ -0,0 +1,20 @@
+import instance from './instance';
+
+const reissueToken = async () => {
+    try {
+        const response = await instance.get('/auth/token/refresh');
+        console.log('reissue Access', response);
+
+        if (response.data.code == 'OK') {
+            const token = response.headers['access-token'];
+            const accessToken = token?.replace('Bearer ', '');
+
+            return accessToken;
+        }
+    } catch (err: any) {
+        console.log('reissue access token error', err);
+        console.log('reissue access erro.response:', err.response);
+    }
+};
+
+export default reissueToken;

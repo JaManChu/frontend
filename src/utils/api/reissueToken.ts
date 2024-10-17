50f798ac78666a7604551031d5106cb981db4e2d
sol2588 <dsk2588@gmail.com>
2024-10-17 19:30:06 +0900

    Fix: accessToken 재발급 - instance 사용

--- src/utils/api/reissueToken.ts
+++ src/utils/api/reissueToken.ts
@@ -1,16 +1,9 @@
 import instance from './instance';
-import { store } from '../../redux/store/store';
 
 const reissueToken = async () => {
     try {
-        const token = store.getState().user.value.token; // 기존 토큰 가져오기
-        const response = await instance.get('/auth/token/refresh', {
-            headers: {
-                'access-token': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
-        console.log('문제야...:', response);
+        const response = await instance.get('/auth/token/refresh');
+        console.log('reissue 문제야...:', response);
 
         if (response.data.code == 'OK') {
             const token = response.headers['access-token'];

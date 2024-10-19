db0760dcbd6847f0bea790fd0dac0ae912271124
sol2588 <dsk2588@gmail.com>
2024-10-19 17:51:03 +0900

    Feat: 무한스크롤 - 전체 적용

--- src/hooks/useSSE.ts
+++ src/hooks/useSSE.ts
@@ -1,52 +1,52 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
+import useAuthToken from './useAuthToken';
 
-export default function useSSE() {
-    // stompjs sockjs
+export default function fetchSSE() {
     const [alarmData, setAlarmData] = useState<string[]>([]); // 서버가 푸쉬한 데이터 저장
 
-    const EventSource = EventSourcePolyfill || NativeEventSource; // request header에 token을 보내기 위해 EventSourcePolyfill
+    const EventSource = EventSourcePolyfill || NativeEventSource; // request header에 token을 보내기 위해 EventSourcePolyfill(EventSource는 header 수정불가)
     const eventSource = useRef<null | EventSourcePolyfill>(null); // 상태변화가 직접적으로 Ui에 영향을 주지 않아 useRef사용
+    const token = useAuthToken();
 
-    useEffect(() => {
-        const fetchSSE = () => {
-            // 새로운 EventSource생성
-            eventSource.current = new EventSource(`${import.meta.env.VITE_BASE_URL}/notify`, {
-                headers: {
-                    'access-token': `Bearer ${sessionStorage.getItem('token')}`,
-                },
-                withCredentials: true,
-            });
+    // 기존 연결이 있는 경우 해제
+    if (eventSource.current) {
+        eventSource.current.close();
+    }
 
-            // 연결
-            eventSource.current.onopen = () => {
-                console.log('연결상태');
-            };
+    // 새로운 EventSource생성
+    eventSource.current = new EventSource(`${import.meta.env.VITE_BASE_URL}/notify`, {
+        headers: {
+            'access-token': `Bearer ${token}`,
+        },
+        withCredentials: true,
+    });
 
-            // 데이터 받아옴
-            eventSource.current.onmessage = (evt) => {
-                console.log('event data: ', evt.data);
-                const parsedData = JSON.parse(evt.data);
-                setAlarmData((prev) => [...prev, parsedData]); // recipeId, comment, reviewer, createdAt
-            };
+    // 연결
+    eventSource.current.onopen = () => {};
 
-            // 종료시 onerror로 처리
-            eventSource.current.onerror = (err: any) => {
-                console.log(err);
-                console.log('error 상태');
-                eventSource.current?.close(); // err이면 종료
+    // 데이터 받아옴
+    eventSource.current.onmessage = async (evt) => {
+        const res = await evt.data;
+        console.log('event data: ', res);
+        const parsedData = JSON.parse(res);
+        // 이벤트 중복체크
+        setAlarmData((prev) => [...prev, parsedData]); // recipeId, comment, reviewer, createdAt
+    };
 
-                setTimeout(fetchSSE, 5000); // 재연결
-            };
-        };
+    // 종료시 onerror로 처리
+    eventSource.current.onerror = (err: any) => {
+        console.log(err);
+        console.log('error 상태');
+        eventSource.current?.close();
+        if (err.error) {
+            // 에러 발생 시 할 일
+        }
 
-        fetchSSE();
-
-        return () => {
-            eventSource.current?.close();
-        };
-    }, []);
-
+        if (err.target.readyState === EventSource.CLOSED) {
+            // 종료 시 할 일
+        }
+    };
     const closeConnection = () => {
         eventSource.current?.close();
         console.log('SSE 연결 종료');
@@ -54,3 +54,5 @@ export default function useSSE() {
 
     return { alarmData, closeConnection };
 }
+// receipeName, message, rating, commentUser
+// 유저당 초기에 1번만 "Alarm Init Message"

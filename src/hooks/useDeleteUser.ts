7a1bb20e8f8e495d1216905dc2b5da70d9384efa
kkim_se <sehun910124@naver.com>
2024-10-24 01:25:32 +0900

    [feat] dispath사용

--- src/hooks/useDeleteUser.ts
+++ src/hooks/useDeleteUser.ts
@@ -1,29 +1,17 @@
+import { useDispatch } from 'react-redux';
 import instance from '../utils/api/instance';
 import { useNavigate } from 'react-router-dom';
+import { logoutSuccess } from '../redux/reducer/userSlice';
 export const useDeleteUser = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const handleDeleteUser = async () => {
         try {
             const response = await instance.delete('/users');
             console.log('response.data : ', response.data);
             if (response.data.code === 'OK') {
-                // 회원 탈퇴 성공 시, 토큰 정보 초기화
-                const persistData = sessionStorage.getItem('persist:root');
-                if (persistData) {
-                    // `persist:root` 데이터 파싱
-                    const parsedData = JSON.parse(persistData);
-
-                    // `user` 정보를 초기화
-                    parsedData.user = JSON.stringify({
-                        value: {
-                            isLoggedIn: false,
-                            token: null,
-                        },
-                    });
-
-                    // 수정된 데이터를 다시 `sessionStorage`에 저장
-                    sessionStorage.setItem('persist:root', JSON.stringify(parsedData));
-                }
+                dispatch(logoutSuccess());
 
                 alert('탈퇴가 성공적으로 완료됐습니다.');
                 navigate('/');
